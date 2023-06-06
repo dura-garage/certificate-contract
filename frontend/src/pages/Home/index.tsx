4bc2b5369c1a20ece85a59500eecfcfb554efe7e
Aryog <yogesh000aryal@gmail.com>
2023-06-06 22:02:22 +0545

    tailwind setup

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='flex items-center justify-center text-blue-400'>Home</div>
     )
 }
 
