6f1d9a92a2d9961de775354b4d611cf07e8ff959
Aryog <yogesh000aryal@gmail.com>
2023-06-06 21:54:54 +0545

    routes and others

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Layout from './components/Navigation/Layout';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import User from './pages/User';
+import Organisation from './pages/Organisation';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/user" element={<User />} />
+            <Route path="/organisation" element={<Organisation />} />
+          </Routes>
+
+        </Layout>
+      </Router>
     </div>
   );
 }
