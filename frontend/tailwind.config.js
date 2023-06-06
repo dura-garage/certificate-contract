61f93ec8deb8252b7df770e7edef6daa14035920
Aryog <yogesh000aryal@gmail.com>
2023-06-06 22:02:35 +0545

    tailwind setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./public/**/*.{html, js, jsx, ts, tsx} ",
+        "./public/*.{html, js, jsx, ts, tsx} ",
+        // "./dist/index.html",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                gray: "#f0f2f5",
+            },
+        },
+        plugins: [],
+    },
+};
